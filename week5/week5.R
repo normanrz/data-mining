bd49912e1696120bede3e8952a582fedf913f291
Norman Rzepka <code@normanrzepka.de>
2015-06-21 13:33:35 +0200

    week5 A7

--- week5/week5.R
+++ week5/week5.R
@@ -3,9 +3,25 @@ printf <- function(...) cat(sprintf(...))
 
 # Ass 7
 ass7 <- function () {
-  model <- lda(data.frame(Sepal=iris$Sepal.Length, Petal=iris$Petal.Length), iris$Species)
   test <- data.frame(Sepal=c(4.5, 5, 5.5, 6, 7), Petal=c(2, 1.5, 3, 4.3, 7))
+  
+  # Implementation 1
+  model <- lda(data.frame(Sepal=iris$Sepal.Length, Petal=iris$Petal.Length), iris$Species)
   print(predict(model, test)$class)
+  
+  # Implementation 2
+  require(mvtnorm)
+  for (i in 1:nrow(test)) {
+    test_row <- test[i,]
+    printf("%f %f %s\n", test_row[,1], test_row[,2], levels(iris$Species)[which.max(lapply(levels(iris$Species), function (k) {
+      rows <- iris[iris$Species == k,]
+      mu <- colMeans(cbind(rows$Sepal.Length, rows$Petal.Length))
+      sig <- cov(cbind(rows$Sepal.Length, rows$Petal.Length))
+      dmvnorm(test_row, mu, sig)[1]
+    }))])
+  }
+  
+  
 }
 ass7()
 
@@ -27,4 +43,4 @@ ass8 <- function () {
                family=binomial(logit), data=menarche)
   lines(menarche$Age, model$fitted, type="l", col="red")
 }
-ass8()
+# ass8()
